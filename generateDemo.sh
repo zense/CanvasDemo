3f5c97549e8b558c43afbc70ed4b1e9d926f9f5f
Shubhayu-Das <Shubhayu64@gmail.com>
2019-11-21 18:25:06 +0530

    Added some submissions

--- generateDemo.sh
+++ generateDemo.sh
@@ -1,6 +1,7 @@
 # To use this, move this file out of the repository
 # Clone the CanvasCompetition repository from Github
 
+cd ../
 rm -rf ./CanvasDemo/examples/*
 rm ./CanvasDemo/CNAME
 rm ./CanvasDemo/index.html
@@ -28,6 +29,21 @@ git fetch --all
 git pull --all
 
 git for-each-ref --format='%(refname)__%(authorname)' refs/heads/ | grep 'HacKnight2K19/.*' | while read branchRef; do
+  tempRef=${branchRef#*refs/heads/}
+  branchName=${tempRef%%__*}
+  authorName=${branchRef#*__}
+  echo "ref: "$tempRef 
+  echo $branchName
+  git checkout $branchName
+  git pull origin $branchName
+  git archive --format=tar --prefix="$branchName/" "$branchName" | tar -C../CanvasDemo -x
+  echo -e "\n\t\t<div class=\"card border-0\">" >> ../CanvasDemo/index.html
+  echo -e "\t\t\t<div class=\"card-body\">\n\t\t\t\t<a href=\"./$branchName/index.html\" class=\"link mr-auto\" target=\"_blank\">Submission: ${branchName:14}</a> <br>" >> ../CanvasDemo/index.html
+  echo -e "\t\t\t\t<a href=\"https://github.com/zense/Canvas-Competition/tree/$branchName\" class=\"link ml-auto\" target=\"_blank\"><i class=\"fab fa-github\"></i></a><br>\n\t\t\t</div>" >> ../CanvasDemo/index.html
+  echo -e "\t\t</div>" >> ../CanvasDemo/index.html
+done
+
+git for-each-ref --format='%(refname)__%(authorname)' refs/heads/ | grep 'hacknight2019/.*' | while read branchRef; do
   tempRef=${branchRef#*refs/heads/}
   branchName=${tempRef%%__*}
   authorName=${branchRef#*__}
